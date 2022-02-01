c4de6eabfaed1bbc7822f160983df0ff63ba488a
rileyantiporda <92815006+rileyantiporda@users.noreply.github.com>
2022-02-01 11:45:18 -0700

    Update detectionSystem.py

--- detectionSystem.py
+++ detectionSystem.py
@@ -7,23 +7,24 @@ def setup():
             reader = mercury.Reader('tmr:///dev/ttyS0')
             reader.set_read_plan([1], 'GEN2')
             reader.set_region('NA2')
-    
+
             return reader
-        
+
         except TypeError:
             print('broke')
 
-    
+
 def detect(r):  # returns the manifest
     # r should be a mercury reader object
     while True:
         try:
             epcs = list(map(lambda t: t.epc.decode(), r.read()))
-        
+
             return {'success': True, 'uuids': epcs}
-        
+
         except TypeError:
             print('timeout during read')
+            return {'success': False, 'uuids': None}
 
 
 if __name__ == '__main__':
